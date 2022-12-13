441edc0ea406aa6ffd49e97fe070bec31dff78d5
Viktor Hryhorchuk <viktor_hryhorchuk@epam.com>
2022-12-13 21:04:16 +0200

    Finilize server

--- src/server/app.py
+++ src/server/app.py
@@ -6,15 +6,24 @@ from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from transformers import pipeline
+import base64
+from PIL import Image
+from io import BytesIO
+
+from transform import transform
+from cats_classifier import get_pred, load_model
 
 
 app = FastAPI()
 
 model_en_checkpoint = os.getenv('MODEL_EN_ABS_PATH')
 model_uk_checkpoint = os.getenv('MODEL_UK_ABS_PATH')
+model_cls_checkpoint = os.getenv('MODEL_ISCAT_ABS_PATH')
 
-# translator_en = pipeline("translation", model=model_en_checkpoint)
-# translator_uk = pipeline("translation", model=model_uk_checkpoint)
+translator_en = pipeline("translation", model=model_en_checkpoint)
+translator_uk = pipeline("translation", model=model_uk_checkpoint)
+
+cls_model = load_model('model_cls_checkpoint')
 
 EN_LANG = 'en'
 UK_LANG = 'uk'
@@ -44,14 +53,18 @@ async def translate(translate_input: TranslateInput) -> List[TranslatedText]:
     if not input_to_translate:
         raise HTTPException(status_code=400, detail="Nothing to translate")
 
-    # result = translator_en(input_to_translate) if source_lang == EN_LANG else translator_uk(input_to_translate)
-    return None  # result
+    result = translator_en(input_to_translate) if source_lang == EN_LANG else translator_uk(input_to_translate)
+    return result
 
 
 @app.post("/classify_cat", response_model=ImageResult)
 async def classify_cat(cat_image: ImageToClassify):
     base64_src = cat_image.src
-    return {'is_cat': False}
+    im = Image.open(BytesIO(base64.b64decode(base64_src)))
+    im_transformed = transform(im)
+    res = get_pred(cls_model, im_transformed)
+    print(res)
+    return {'is_cat': res == 'cat'}
 
 
 app.mount("/", StaticFiles(directory="build", html=True), name="build")
---
+++ src/server/cats_classifier.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+
+
+classes = {
+    0: 'cat',
+    1: 'dog'
+}
+
+
+class ImageNet(nn.Sequential):
+
+    def __init__(self):
+        super().__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, padding=0, stride=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, padding=0, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=0, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.fc1 = nn.Linear(3 * 3 * 64, 10)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(10, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1)
+        out = self.relu(self.fc1(out))
+        out = self.fc2(out)
+        return out
+
+
+def load_model(model_save_path):
+    model_params = torch.load(model_save_path, map_location=torch.device('cpu'))
+    model_loaded = ImageNet()
+    model_loaded.load_state_dict(model_params)
+    return model_loaded
+
+
+def get_pred(model, image):
+    with torch.no_grad():
+        image = image.unsqueeze(0)
+        pred = model(image)
+        _, result = torch.max(pred, 1)
+
+    print(f'Predicted {result}.')
+    print(f'Predicted {classes[result.item()]}.')
+    return classes[result.item()]
